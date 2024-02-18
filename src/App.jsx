8b34d361d4bff648d8248310b4fda10060c74aef
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-02-18 16:30:47 +0300

    using state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
-import { useState } from "react"
-import "./App.css"
+import { useState } from "react";
+import "./App.css";
 
-export default function App(){
-  const [count,setCount] = useState(0)
+export default function App() {
+  const [tasks, setTask] = useState([
+    { id: 5271, name: "Record React Lectures", completed: true },
+    { id: 7825, name: "Edit React Lectures", completed: false },
+    { id: 8391, name: "Watch Lectures", completed: false },
+  ]);
+  const [show, setShow] = useState(true);
+
+  let del = (id) => {
+    setTask(tasks.filter((count) => id !== count.id));
+  };
+
   return (
     <>
-      <div className="board">
-        {count}
-        <button onClick={() => setCount(count + 3)}>count</button>
+      <div className="App">
+        <ul>
+          <button className="trigger" onClick={() => setShow(!show)}>
+            Toggle
+          </button>
+          {show &&
+            tasks.map((count) => (
+              <li key={count.id}>
+                <p>{count.name} </p>
+                <button
+                  className="delete"
+                  onClick={() => {
+                    del(count.id);
+                  }}
+                >
+                  Delete
+                </button>
+              </li>
+            ))}
+        </ul>
       </div>
     </>
   );
