16af590f280df5a28aa643f910c5a79fd2a212a5
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-02-19 13:59:52 +0300

    Added a reset button and updated the styling of the add task form.

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import "./AddTask.css";
 
 export default function AddTask() {
+  const [taskValue, setTaskValue] = useState("");
   return (
     <section className="addtask">
       <form>
-        <label htmlFor="task">Task Name</label>
-        <input type="text" onChange={() => {console.log(event)}} name="task" id="task" required />
+        <label htmlFor="task">Task Name :</label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setTaskValue(e.target.value);
+          }}
+          name="task"
+          id="task"
+          autoComplete="off"
+          required
+        />
         <button type="submit">Add Task</button>
+        <span className="reset">Reset</span>
       </form>
     </section>
   );
